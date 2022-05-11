0fb6ea24dd80286f70ca9fff5e2b732345496a3e
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-05-12 00:52:44 +0300

    Add project files.

---
+++ recurs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace recurs
+{
+    internal class Program
+    {
+        static void PrintArray(int[] myArray, int i = 0)
+        {
+            if (i >= myArray.Length)
+                return;
+
+            PrintArray(myArray, i + 1);
+
+            Console.WriteLine(myArray[i]);
+
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int[] myArray = { 3, 4, 8 };
+
+            PrintArray(myArray);
+        }
+    }
+}
